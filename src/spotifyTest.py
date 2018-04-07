be1748b8e6c070ce2dec63343774bfe49b3cf927
Usman Rafi <usman.rafi.96@gmail.com>
2018-04-07 09:57:47 +0500

    Done -> Current playing song on Spotify

--- src/spotifyTest.py
+++ src/spotifyTest.py
@@ -1,7 +1,11 @@
 import os
 import sys
+import json
+
 import spotipy
 import spotipy.util as util
+
+import YoutubeSearcher as yt
 
 def getCredentials(filename):
 	fin = open(filename).read()
@@ -15,4 +19,14 @@ token = util.prompt_for_user_token(username, scope, client_id, client_secret, re
 
 sp = spotipy.Spotify(token)
 
-print(sp.current_user_playing_track())
+resp = json.loads(json.dumps(sp.current_user_playing_track()))
+details = resp["item"]
+
+album_name = details["album"]["name"] 
+song_name = details["name"]
+artists_name = details["artists"][0]["name"]
+
+print(song_name)
+print(artists_name)
+
+yt.searchAndDl(artists_name + " " + song_name, "mp3")
